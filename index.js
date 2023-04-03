24ef6359a000da6177689610dd747fc970612eeb
Robert Arscott <robertarscott1993@gmail.com>
2023-04-02 22:34:25 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.round(Math.random()*100);
+
+let num3 = 17;
+let num4 = 13;
+
+let mod = num3 % num4;
+
+let max = Math.max(3,4,2,20)
